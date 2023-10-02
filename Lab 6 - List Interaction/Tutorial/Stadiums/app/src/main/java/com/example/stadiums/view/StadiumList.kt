6592a8495d2e73c620064f70f027ab6f198d2c4e
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-02 10:55:37 +0300

    stadiumRepo

--- Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/view/StadiumList.kt
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/view/StadiumList.kt
@@ -1,36 +1,44 @@
 package com.example.stadiums.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.stadiums.R
 import com.example.stadiums.model.Stadium
+import com.example.stadiums.repo.StadiumRepo
 import com.example.stadiums.ui.theme.StadiumsTheme
 
 @Composable
 fun StadiumCard(stadium: Stadium) {
+//    image id from image name
+    val imageId = LocalContext.current.resources.getIdentifier(
+        stadium.imageName,
+        "drawable",
+        LocalContext.current.packageName
+    )
+
     Card {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.padding(10.dp)
         ) {
             Image(
-                painter = painterResource(id = R.drawable.al_gharafa),
+                painter = painterResource(id = imageId),
                 contentDescription = null,
                 modifier = Modifier
                     .size(160.dp)
@@ -51,30 +59,22 @@ fun StadiumCard(stadium: Stadium) {
 
 @Composable
 fun StadiumList(stadiums: List<Stadium>) {
-
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        contentPadding = PaddingValues(10.dp)
+    ) {
+        items(stadiums) {
+            StadiumCard(stadium = it)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun StadiumListPreview() {
-    val stadiums = listOf(
-        Stadium(
-            name = "Al Gharafa",
-            city = "Doha",
-            imageName = "al_gharafa",
-            seatingCapacity = 100000,
-            status = "Built"
-        ),
-        Stadium(
-            name = "Al Shamal",
-            city = "Doha",
-            imageName = "al_shamal",
-            seatingCapacity = 100000,
-            status = "Built"
-        )
-    )
+    val stadiums = StadiumRepo.getStadiums(LocalContext.current)
     StadiumsTheme {
-
+        StadiumList(stadiums)
     }
 }
 
