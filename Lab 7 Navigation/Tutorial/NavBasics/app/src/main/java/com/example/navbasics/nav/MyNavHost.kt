e4016952b209577ff8f4af69d321f1d16d2d3b3b
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-09 10:26:26 +0300

    updated code

--- Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/nav/MyNavHost.kt
+++ Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/nav/MyNavHost.kt
@@ -5,15 +5,23 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.navbasics.ui.screen.Screen
+import com.example.navbasics.ui.screen.Screen1
+import com.example.navbasics.ui.screen.Screen2
 
 @Composable
-fun MyNavHost(navController: NavHostController , paddingValues: PaddingValues){
+fun MyNavHost(navController: NavHostController, paddingValues: PaddingValues) {
     NavHost(
         navController = navController,
         startDestination = Screen.Screen1.route
     ) {
 //        do the mapping
-
+        composable(route = Screen.Screen1.route){
+            Screen1()
+        }
+        composable(route = Screen.Screen2.route){
+            Screen2()
+        }
     }
 }
