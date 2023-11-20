9e5526518cedbe7d411a9dd75fe079771f0d55f5
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-11-20 10:01:02 +0300

    updated

--- Lab 11 Firebase/TodoListApp/app/src/main/java/com/cmps312/todolistapp/model/Project.kt
+++ Lab 11 Firebase/TodoListApp/app/src/main/java/com/cmps312/todolistapp/model/Project.kt
@@ -5,7 +5,6 @@ import com.google.firebase.firestore.DocumentId
 data class Project(
     var name: String,
 
-    @DocumentId
     val id: String = "",
 ){
     constructor() : this("") {}
--- Lab 11 Firebase/TodoListApp/app/src/main/java/com/cmps312/todolistapp/repository/TodoRepository.kt
+++ Lab 11 Firebase/TodoListApp/app/src/main/java/com/cmps312/todolistapp/repository/TodoRepository.kt
@@ -11,72 +11,23 @@ import kotlinx.coroutines.tasks.await
 
 class TodoRepository {
 
-    private val db: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
-    private val projectDocumentsRef by lazy { db.collection("projects") }
-    private val todoDocumentsRef by lazy { db.collection("todos") }
 
-    fun observeProjects(): Flow<List<Project>> = callbackFlow {
-        val snapshot = projectDocumentsRef
-            .addSnapshotListener { value, e ->
-                if (e != null)
-                    return@addSnapshotListener
-                val projects = value!!.toObjects(Project::class.java)
-                if (projects != null)
-                    this.trySend(projects)
-            }
-        awaitClose {
-            snapshot.remove()
-        }
-    }
+    fun observeProjects(): Flow<List<Project>>
 
-    fun addProject(project: Project) = projectDocumentsRef.add(project)
-        .addOnSuccessListener { Log.d("TAG", "Successfully added project: ") }
-        .addOnFailureListener { Log.d("TAG", "Failed to add project: ") }
+    fun addProject(project: Project)
+
+    fun updateProject(project: Project)
+
+    suspend fun deleteProject(project: Project)
+
+    fun observeTodos(pid: String): Flow<List<Todo>>
+
+    suspend fun getTodo(id: String):
+    suspend fun addTodo(todo: Todo)
 
 
-    fun updateProject(project: Project) = projectDocumentsRef.document(project.id).set(project)
+    fun updateTodo(todo: Todo)
+    fun deleteTodo(todo: Todo)
 
-    suspend fun deleteProject(project: Project) {
-        val todos = getTodoListByProject(project.id)
-        todos.forEach { todo ->
-            deleteTodo(todo).await()
-        }
-        projectDocumentsRef.document(project.id).delete().await()
-    }
-
-    fun observeTodos(pid: String): Flow<List<Todo>> = callbackFlow {
-        val snapshot = todoDocumentsRef
-            .whereEqualTo("pid", pid)
-            .addSnapshotListener { value, e ->
-                if (e != null) {
-                    Log.d("observeTodos", e.stackTraceToString())
-                    return@addSnapshotListener
-                }
-                val todos = value!!.toObjects(Todo::class.java)
-                if (todos.isNotEmpty())
-                    this.trySend(todos)
-            }
-        awaitClose {
-            snapshot.remove()
-        }
-    }
-
-    suspend fun getTodo(id: String): Todo =
-        todoDocumentsRef
-            .document(id)
-            .get().await().toObject(Todo::class.java)!!
-
-    suspend fun addTodo(todo: Todo) =
-        todoDocumentsRef.add(todo)
-            .addOnSuccessListener { Log.d("TAG", "Successfully added: ") }
-            .addOnFailureListener { Log.d("TAG", "Failed: ") }
-
-    fun updateTodo(todo: Todo) = todoDocumentsRef.document(todo.id).set(todo)
-    fun deleteTodo(todo: Todo) = todoDocumentsRef.document(todo.id).delete()
-
-    suspend fun getTodoListByProject(pid: String): List<Todo> = todoDocumentsRef
-        .whereEqualTo("pid", pid)
-        .get()
-        .await()
-        .toObjects(Todo::class.java)
+    suspend fun getTodoListByProject(pid: String)
 }
